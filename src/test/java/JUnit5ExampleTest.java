55df4720198b3a4927fe27e66208b13f9c60ca6b
Evgenii_Lartcev <Evgenii_Lartcev@epam.com>
2017-09-12 17:08:17 +0300

    first commit

---
+++ src/test/java/JUnit5ExampleTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author (created on 9/12/2017).
+ */
+public class JUnit5ExampleTest {
+
+    @Test
+    void justAnExample() {
+        System.out.println("This test method should be run");
+    }
+}
