8fb3da1472bc3323810b2b9929a018b527935cec
Evgenii_Lartcev <Evgenii_Lartcev@epam.com>
2017-09-12 17:55:41 +0300

    quick examples

--- src/test/java/JUnit5ExampleTest.java
+++ src/test/java/JUnit5ExampleTest.java
@@ -1,4 +1,10 @@
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 
 /**
  * @author (created on 9/12/2017).
@@ -6,7 +12,31 @@ import org.junit.jupiter.api.Test;
 public class JUnit5ExampleTest {
 
     @Test
+    @DisplayName("Custom test name containing spaces")
     void justAnExample() {
         System.out.println("This test method should be run");
+    }
+
+    @Test
+    @DisplayName("😱")
+    void testWithDisplayNameContainingEmoji() {
+    }
+
+    @Test
+    void groupedAssertions() {
+        // In a grouped assertion all assertions are executed, and any
+        // failures will be reported together.
+        assertAll("person",
+                () -> assertEquals("John", "John"),
+                () -> assertEquals("Doe", "Doe")
+        );
+    }
+
+    @Test
+    void exceptionTesting() {
+        Throwable exception = assertThrows(IllegalArgumentException.class, () -> {
+            throw new IllegalArgumentException("a message");
+        });
+        assertEquals("a message", exception.getMessage());
     }
 }
